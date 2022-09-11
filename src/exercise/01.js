5aba50ea1a86f40b9f437369837530d55a6c887f
Steven Lawson <stevenleelawson@gmail.com>
2022-09-11 11:50:50 -0600

    setting initialValue default to an empty string so component is always controlled

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-function Greeting({initialValue}) {
+function Greeting({initialValue = ''}) {
   const [name, setName] = React.useState(initialValue)
 
   function handleChange(event) {
